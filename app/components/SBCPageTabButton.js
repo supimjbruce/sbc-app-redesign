3796def49d2a557502fd338e644c4eaac8f4e672
Jonathan Bruce <supimjbruce@gmail.com>
2025-06-07 18:12:20 -0500

    Created SBCPageTabButton.js
    Wanted to make a simple Component that can be reused for each and every tab bar button that goes to a page. Going to keep working on this!
    
    .j.

---
+++ app/components/SBCPageTabButton.js
@@ -0,0 +1,22 @@
+/* sbc-app-redesign */
+/* Home Page [index.js] */
+
+/*By Jonathan Bruce
+JBruce, LLC
+jbruce.design
+.j.
+- - - - - */
+
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+
+export default function SBCPageTabButton({tabName}) {
+    return (
+        <View>
+            <Text>
+                {/*src={getImageUrl(iconImageSource)}*/}
+            </Text>
+            <Text>{tabName}</Text>
+        </View>
+    );
+};
