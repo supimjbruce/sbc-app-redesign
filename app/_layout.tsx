198a232058a8890e43760ad6f0b9aee05d2f8c0d
Jonathan Bruce <supimjbruce@gmail.com>
2025-06-30 14:22:40 -0400

    Updated _layout.tsx
    Updated this so it adds in Stack Screens and also so that it takes the header out of the index.js

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,21 @@
+/* sbc-app-redesign */
+/* Overall Layout File [_layout.tsx] */
+
+/*By Jonathan Bruce
+JBruce, LLC
+jbruce.design
+.j.
+- - - - - */
+
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
-}
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      {/*<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" options={{ headerShown: false }} />*/}
+    </Stack>
+  );
+}; /* Think about how to best organize the tabs/screens for this app!!
+Do I want the most important tabs at the bottom and everything else on a tab bar? */
